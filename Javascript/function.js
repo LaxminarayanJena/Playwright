1e2ac63f11fe28b3f501e8cc22ff6311389b2509
LaxminarayanJena <lnarayan.jena@gmail.com>
2026-01-08 19:42:07 +0530

    JavaScript Basics

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,4 +1,26 @@
-function add()
+function add(a,b)
 {
-    
+    return a+b
 }
+
+let sum=add(2,3)
+console.log(sum)
+
+//anonymous function -no name
+//anonymous can be assigned to variable
+
+let sumofintegers=function(c,d)
+{
+    return c+d
+}
+console.log(sumofintegers(2,9))
+
+//fatpipe
+let sumofNumbers=(c,d)=> c+d
+console.log(sumofNumbers(2,6))
+
+
+
+
+
+
