ad0f705eda90716d2b4f84720980276cc1b79920
LaxminarayanJena <lnarayan.jena@gmail.com>
2026-01-10 11:03:36 +0530

    PlayWright Basics

--- Automation/tests/FirstTest.spec.js
+++ Automation/tests/FirstTest.spec.js
@@ -33,21 +33,73 @@ test('Page Playwright test' , async ({page})=>
 });
 
 
-test.only('practice Context Playwright test' , async ({browser})=>
+test('practice Context Playwright test' , async ({browser})=>
 {
     const context= await browser.newContext();
     const page =await context.newPage();
     const password= page.locator("[type='password']")
     const error= page.locator("[id*='error']")
+    const cardTitles= page.locator(".card-body a")
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await page.getByRole('textbox', { name: 'Username:' }).fill("rahulshettyacademy");
     await password.fill("learning");
     await await page.getByRole('button', { name: 'Sign In' }).click();
-    console.log(await page.locator(".card-body a").first().textContent());
-    console.log(await page.locator(".card-body a").nth(1).textContent());
+    console.log(await cardTitles.first().textContent());
+    console.log(await  cardTitles.nth(1).textContent());
+   // await page.waitForLoadState('networkidle');//discouraged
+    //await cardTitles.waitFor(); //encouraged
+    //await cardTitles.first().waitFor(); //encouraged
+    const allTitles =await cardTitles.allTextContents() //playwright will not wait
+    console.log(allTitles)
+
+    //await page.waitForTimeout(6000); // waits for 2 seconds
 
 
-    await page.waitForTimeout(6000); // waits for 2 seconds
+});
+
+
+
+
+test.only('UI CONTROL' , async ({browser})=>
+{
+    const context= await browser.newContext();
+    const page =await context.newPage();
+    const password= page.locator("[type='password']")
+    const error= page.locator("[id*='error']")
+    const cardTitles= page.locator(".card-body a")
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.getByRole('textbox', { name: 'Username:' }).fill("rahulshettyacademy");
+    await password.fill("learning");
+    const dropdown =page.locator("select.form-control");
+    dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+     await page.locator("#terms").click();
+     await expect(page.locator("#terms")).toBeChecked();
+     await page.locator("#terms").uncheck();
+     expect( await page.locator("#terms").isChecked()).toBeFalsy();
+
+
+
+
+
+
+
+
+
+
+    //await await page.getByRole('button', { name: 'Sign In' }).click();
+    //console.log(await cardTitles.first().textContent());
+    //console.log(await  cardTitles.nth(1).textContent());
+   // await page.waitForLoadState('networkidle');//discouraged
+    //await cardTitles.waitFor(); //encouraged
+    //await cardTitles.first().waitFor(); //encouraged
+    //const allTitles =await cardTitles.allTextContents() //playwright will not wait
+    //console.log(allTitles)
+
+    await page.waitForTimeout(3000); // waits for 2 seconds
 
 
 });
